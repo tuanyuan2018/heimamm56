e499e90ea4c6f7039bda13da760479cc7142685e
tuanyuan2019 <2453684295@qq.com>
2020-07-08 19:35:42 +0800

    项目起步-element-ui整合

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+      <el-button>默认按钮</el-button>
+  </div>
 </template>
 <script>
 export default {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入element-ui插件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册element-ui
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
