9311acb053a467185a8d7a73c786046e9a83bb01
tuanyuan2019 <2453684295@qq.com>
2020-07-08 19:19:17 +0800

    项目起步-删除默认代码

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
-
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
-
 <style>
-@import url(~@/assets/base.css);
 </style>
