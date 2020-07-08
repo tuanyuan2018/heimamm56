45e2b37248a241eecd3f3a7d987110b5a08121da
tuanyuan2019 <2453684295@qq.com>
2020-07-08 20:18:40 +0800

    登录模块-路由整合

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
       <el-button>默认按钮</el-button>
+      <router-link to="">跳转</router-link>
+      <router-view></router-view>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 // 注册element-ui
 Vue.use(ElementUI);
-
+// 导入路由js
+import router from '@/router/router.js';
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
